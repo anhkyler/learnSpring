2db440db9ed2e0ad0a4d89426bc91373fb65734a
hieu so <hieuphso@gmail.com>
2021-01-28 22:40:24 -0500

    initial commit

---
+++ main/java/com/hieuso/learning/springboot/webrestfulservices/WebRestfulServicesApplication.java
@@ -0,0 +1,13 @@
+package com.hieuso.learning.springboot.webrestfulservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRestfulServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebRestfulServicesApplication.class, args);
+	}
+
+}
---
+++ test/java/com/hieuso/learning/springboot/webrestfulservices/WebRestfulServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hieuso.learning.springboot.webrestfulservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRestfulServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
